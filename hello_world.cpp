c8f9622a46ce895733bfff52010d4eb7b28704f0
Sveta1305 <satim2502@mail.ru>
2022-03-18 13:36:56 +0300

    Comment Hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-char[200] name;
+char[200] name; // Имя пользователя
 std::cout << "Enter name user: " << std::endl;
 std::cin >> name;
 std::cout << "Hello world from " << name << " !" << std::endl;
