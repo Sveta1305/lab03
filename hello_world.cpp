e9c3b144f78339992bbc7f4177f3118139d2aa6c
Sveta1305 <89978320+Sveta1305@users.noreply.github.com>
2022-03-19 13:43:34 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 int main(){
 char[200] name; // Имя пользователя,сделанный в гитхабе
 std::cout << "Enter name user: " << std::endl;
-std::cin >> name;
+std::cin >> name; // имя пользователя
 std::cout << "Hello world from " << name << " !" << std::endl;
 return 0;
 }
